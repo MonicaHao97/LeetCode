201814d892b16ff2761c12439c33f55134b53bca
MonicaHao97 <hao.yo@northeastern.edu>
2021-04-10 02:41:07 -0700

    19-1

---
+++ 19-M-LL-RemoveFromEnd.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode temp = head;
+        ListNode point = new ListNode();
+        point.next = head;
+        int count = 0;
+        
+        while(temp != null) {
+            temp = temp.next;
+            count ++;
+        }
+        
+        if(count == n) {
+            head = head.next;
+            return head;
+        }
+        
+        temp = head;
+        for(int i = 0; i < count - n; i ++){
+            temp = temp.next;
+            point = point.next;
+        }
+        point.next = temp.next;
+        
+        return head;
+    }
+}
