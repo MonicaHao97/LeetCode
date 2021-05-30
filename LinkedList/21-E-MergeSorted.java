a3fa3c8a65a8662aedbc374f562179e0c539d646
MonicaHao97 <hao.yo@northeastern.edu>
2021-05-30 03:53:05 -0700

    21-2

--- LinkedList/21-E-MergeSorted.java
+++ LinkedList/21-E-MergeSorted.java
@@ -1,37 +1,19 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null && l2 == null){
-           return l1;
-        }
-        else if(l1 == null) {
+        if(l1 == null) {
             return l2;
         }
-        else if(l2 == null){
+        
+        if(l2 == null) {
             return l1;
         }
         
-        ListNode temp = new ListNode(0);
-        ListNode result = temp;
-        while(l1 != null && l2 != null){
-            if(l1.val < l2.val){
-                result.next = l1;
-                l1 = l1.next;
-            }
-            else{
-                result.next = l2;
-                l2 = l2.next;
-            }
-            result = result.next;
+        if(l1.val <= l2.val) {
+            l1.next =  mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
         }
-        
-        if(l1 != null){
-            result.next = l1;
-        }
-        else{
-            result.next = l2;
-        }
-        
-        return temp.next;
-
     }
 }
