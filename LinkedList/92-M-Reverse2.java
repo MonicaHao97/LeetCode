4ca6037d7e96c7c9c402e15280914d20dbe985a8
MonicaHao97 <hao.yo@northeastern.edu>
2021-05-30 03:51:32 -0700

    92-2

--- LinkedList/92-M-Reverse2.java
+++ LinkedList/92-M-Reverse2.java
@@ -1,23 +1,35 @@
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if(head == null){
+        if(head == null || head.next == null || left == right) {
             return head;
         }
-        ListNode newHead = new ListNode(0);
-        newHead.next = head;
-        ListNode prev = newHead;
-        for(int i = 0; i < left - 1; i ++){
+        
+        ListNode start = head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        
+        for(int i = 0; i < left - 1; i ++) {
+            start = start.next;
             prev = prev.next;
         }
-        ListNode start = prev.next;         
+        
+        //prev and start does not change
+        //only after - the node to be reversed will change
+        //pick t, delete it and insert it to the next of prev
+        //so that the sublist will always be in descending order
         ListNode after = start.next;
-
-        for(int i = 0; i < right - left; i ++){
-         start.next = after.next;
-            after.next  = prev.next;
+        for(int i = 0; i < right-left; i ++) {
+            //skip after
+            start.next = after.next;
+            //move after to front of sublist
+            after.next = prev.next;
+            //connect after and prev;
             prev.next = after;
+            //move after forward;
             after = start.next;
         }
-        return newHead.next;
+        
+        return dummy.next;
     }
 }
