5acffa33f6b075edc93f218d5310cd8d1e56547d
MonicaHao97 <hao.yo@northeastern.edu>
2021-06-08 21:26:16 -0700

    328-2

--- LinkedList/328-M-OddEven.java
+++ LinkedList/328-M-OddEven.java
@@ -1,20 +1,21 @@
 class Solution {
     public ListNode oddEvenList(ListNode head) {
         if(head == null) {
-            return head;
+            return null;
         }
         
         ListNode odd = head;
         ListNode even = head.next;
-        ListNode evenHead = even;
+        ListNode evenhead = even;
         
-        while(odd.next != null && even.next != null) {
+        while(even != null && even.next != null) {
             odd.next = odd.next.next;
             even.next = even.next.next;
             odd = odd.next;
             even = even.next;
         }
-        odd.next = evenHead;
+        odd.next = evenhead;
+        
         return head;
     }
 }
