d81644249b0bee5723a7412a1ecbde8b6805c905
MonicaHao97 <hao.yo@northeastern.edu>
2021-06-11 22:59:18 -0700

    160-2

--- LinkedList/160-E-Intersection.java
+++ LinkedList/160-E-Intersection.java
@@ -1,39 +1,15 @@
-/**
- Definition for singly-linked list.*/
- public class ListNode {
-     int val;
-     ListNode next;
-      ListNode(int x) {
-          val = x;
-          next = null;
-      }
-  }
- 
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode tempA = headA;
-        ListNode tempB = headB;
-        int count = 0;
-        
-        if (headA == null || headB == null){
+        if(headA == null || headB == null) {
             return null;
         }
         
-        while(tempA != tempB && count < 3){
-            if (tempA == null){
-                count ++;
-                tempA = headB;
-            }
-            else{
-                tempA = tempA.next;
-            }
-            if(tempB == null){
-                count ++;
-                tempB = headA;
-            }
-            else{
-                tempB = tempB.next;
-            }
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+        
+        while(tempA != tempB) {
+            tempA = tempA == null ? headB : tempA.next;
+            tempB = tempB == null ? headA : tempB.next;
         }
         return tempA;
     }
