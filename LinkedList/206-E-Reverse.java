421efe57fbd5483fa32594540e8078dd707d0261
MonicaHao97 <hao.yo@northeastern.edu>
2021-06-12 20:41:06 -0700

    206-2

--- LinkedList/206-E-Reverse.java
+++ LinkedList/206-E-Reverse.java
@@ -1,22 +1,13 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode newHead = null;
-        while(head != null){
-            ListNode temp = head.next;
-            head.next = newHead;
-            newHead = head;
-            head = temp;
+        ListNode temp = head;
+        ListNode prev = null;
+        while(temp != null) {
+            ListNode next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
         }
-        return newHead;
+        return prev;
     }
 }
