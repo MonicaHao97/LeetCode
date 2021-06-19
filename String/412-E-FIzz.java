b8caca26b5e381a2cdc6731349be809f59902014
MonicaHao97 <hao.yo@northeastern.edu>
2021-06-18 21:04:01 -0700

    412-2

--- String/412-E-FIzz.java
+++ String/412-E-FIzz.java
@@ -1,22 +1,39 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        List<String> result = new ArrayList<String> ();
-        for (int i = 0; i < n; i++){
-            int test = i + 1;
-            if (test % 15 == 0){
-                result.add(i,"FizzBuzz");
+        //Create String ArrayList for result
+        List<String> result = new ArrayList<String>(n);
+        
+        //Use for loop to track each int
+        //Use fizz and buzz for threshold
+        for(int i = 1, fizz = 0, buzz = 0; i <= n; i ++) {
+            //Increate threshold num firstly
+            fizz ++;
+            buzz ++;
+            
+            //Check for 15's case first
+            //Only when 3 and 5 arrives at same num
+            if(fizz == 3 && buzz == 5) {
+                //Add String and clear the nums
+                result.add("FizzBuzz");
+                fizz = 0;
+                buzz = 0;
             }
-            else if(test % 5 == 0){
-                result.add(i,"Buzz");
+            //Check for fizz
+            else if(fizz == 3) {
+                result.add("Fizz");
+                fizz = 0;
             }
-            else if(test % 3 == 0){
-                result.add(i,"Fizz");
-            }
-            else{
-                String element = test + "";
-                result.add(i, element);
+            //Check for buzz
+            else if(buzz == 5) {
+                result.add("Buzz");
+                buzz = 0;
+            } 
+            //Add String to Int values
+            else {
+                result.add(String.valueOf(i));
             }
         }
+        
         return result;
     }
 }
