0ca3ececf3f317bce7ed337b94ae8c333dfd114e
MonicaHao97 <hao.yo@northeastern.edu>
2021-06-21 21:05:49 -0700

    383-2

--- String/383-E-RansomNote.java
+++ String/383-E-RansomNote.java
@@ -1,12 +1,21 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] freq = new int[26];
+        //Create an int array of length 26
+        //Each to store value for a distinct character
+        int[] letters = new int[26];
         
+        //ransomNote <= magazine in true case
+        //Starts with magazine letters
+        //Each letter finds its position in array and assign value
         for(int i = 0; i < magazine.length(); i ++) {
-            freq[magazine.charAt(i) - 'a'] ++;
+            letters[magazine.charAt(i) - 'a'] ++;
         }
+        
+        //Each value in ransomNote finds its position and minus value
+        //If < 0  means magazine did not contain this letter
+        //Return false; else return true after looping
         for(int i = 0; i < ransomNote.length(); i ++) {
-            if(--freq[ransomNote.charAt(i) - 'a'] < 0) {
+            if(--letters[ransomNote.charAt(i) - 'a'] < 0) {
                 return false;
             }
         }
