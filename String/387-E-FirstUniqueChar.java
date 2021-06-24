812f52a592b1a8c5bc1086b721a54d59a9813682
MonicaHao97 <hao.yo@northeastern.edu>
2021-06-23 22:06:57 -0700

    387-2

--- String/387-E-FirstUniqueChar.java
+++ String/387-E-FirstUniqueChar.java
@@ -1,15 +1,20 @@
 class Solution {
     public int firstUniqChar(String s) {
-        int [] freq = new int[26];
+        //Use letters dictionary
+        int[] letters = new int[26];
         
+        //Counts each letter by ++
         for(int i = 0; i < s.length(); i ++) {
-            freq[s.charAt(i) - 'a'] ++;
+            letters[s.charAt(i) - 'a'] ++;
         }
+        //Check the first letter In String with value 1
+        //return index else -1
         for(int i = 0; i < s.length(); i ++) {
-            if(freq[s.charAt(i) - 'a'] == 1) {
+            if(letters[s.charAt(i) - 'a'] == 1) {
                 return i;
             }
         }
+        
         return -1;
     }
 }
